6b24ab36634e85111e7322c2c5c7f65599873692
sa <sixsecondfiesta@gmail.com>
2016-05-23 04:37:45 -0400

    actually works

--- index.js
+++ index.js
@@ -14,50 +14,54 @@ const getLocalName = (uri) => url.parse(uri).pathname.split('/').slice(1).join('
 
 const gams = fs.readFileSync('gams.txt').toString().split('\n')
 
-const searchUris = gams.slice(0, 3)
+const searchUris = gams.slice(0, 13)
   .map(g => 'http://store.steampowered.com/search/?snr=1_4_4__12&term=' + encodeURI(g))
 
 async.waterfall(
   [
     (cb) => async.mapLimit(
-      searchUris,
+      searchUris.map((v, i) => ({val: v, ind: i})),
       2,
-      (searchUri, cb2) => x(searchUri, '.search_result_row@href')((e, href) => cb2(e, href)),
+      (searchUri, cb2) => {
+        console.log(`Searching game (${searchUri.ind + 1} / ${searchUris.length}) "${searchUri.val}"...`)
+        x(searchUri.val, '.search_result_row@href')((e, href) => cb2(e, href))
+      },
       (e, uris) => { console.log('Got ' + uris.length + ' uris!'); cb(e, uris) }
     ),
-    // (uris, cb) => { const err = uris.reduce((p, c) => p || !c.startsWith(''), false); cb()}
-    // (uris, cb) => { console.log(uris.join('\n')); cb(null, uris) },
     (uris, cb) => async.mapLimit(
-      uris,
+      uris.map((v, i) => ({val: v, ind: i})),
       2,
-      (uri, cb2) => { console.log(uri); x(uri, {
-        details: '.details_block',
-        img: 'img.game_header_image_full@src',
-        revs: '.user_reviews_summary_row[itemprop="aggregateRating"]@data-store-tooltip'
-      })((e, data) => {
-        console.log('here ' + data)
-        const details = data.details
-          .split('\n')
-          .map(txt => txt.trim())
-          .filter(txt => txt.length > 0)
-        
-        cb2(e, {
-          name: getInDataBlock(details, 'Title'),
-          releaseDate: getInDataBlock(details, 'Release Date'),
-          genres: getInDataBlock(details, 'Genre').split(',').trim(),
-          developer: getInDataBlock(details, 'Developer'),
-          publisher: getInDataBlock(details, 'Publisher'),
-          url: uri,
-          img: data.img,
-          popularity: parseInt(data.revs.split(' ')[4], 10),
-          rating: parseInt(data.revs.split(' ')[0], 10)
+      (uri, cb2) => {
+        console.log(`Scraping page (${uri.ind + 1} / ${uris.length}) "${uri.val}"...`)
+        x(uri.val, {
+          details: '.details_block',
+          img: 'img.game_header_image_full@src',
+          revs: '.user_reviews_summary_row[itemprop="aggregateRating"]@data-store-tooltip'
+        })((e, data) => {
+          const details = data.details
+            .split('\n')
+            .map(txt => txt.trim())
+            .filter(txt => txt.length > 0)
+          
+          const revs = data.revs.split(' ')
+          
+          cb2(e, {
+            name: getInDataBlock(details, 'Title'),
+            releaseDate: getInDataBlock(details, 'Release Date'),
+            genres: getInDataBlock(details, 'Genre').split(',').map(g => g.trim()),
+            developer: getInDataBlock(details, 'Developer'),
+            publisher: getInDataBlock(details, 'Publisher'),
+            url: uri.val,
+            img: data.img,
+            popularity: parseInt(revs[3].split(',').join(''), 10),
+            rating: parseInt(revs[0], 10)
+          })
         })
-      }) },
+      },
       (e, data) => { console.log('Got ' + data.length + ' games!'); cb(e, data) }
     ),
     (data, cb) => async.parallelLimit(data.map((d, ind) => (cb2) => {
-      console.log('Downloading image (' + (ind + 1) + '/' + data.length + ') "' + d.img + '"...')
-      console.log(getLocalName(d.img))
+      console.log(`Downloading image (${ind + 1} / ${data.length}) "${d.img}"...`)
       new Download()
         .get(d.img)
         .rename(getLocalName(d.img))
@@ -67,7 +71,7 @@ async.waterfall(
     (games, cb) => cb(null, games.map(g => Object.assign({}, g, { img: path.join('data/images', encodeURI(getLocalName(g.img))) }))),
     (games, cb) => fs.outputFile('data/games.js', 'window.games = ' + JSON.stringify(games, null, 2), cb)
   ],
-  (err) => { /*if(err)*/ { console.error(err) } console.log('Done!') }
+  (err) => { if(err) { console.error(err) } console.log('Done!') }
 )
 
 function getInDataBlock(datablock, keyword) {
